76cc282c3b75a13c23c05d8207557a61b0759c03
DESKTOP-PA8N0K6\user <songbyoungcheol@gmail.com>
2022-04-03 21:17:56 +0900

    CH04_실습

---
+++ CH04/CH04-실습.py
@@ -0,0 +1,5 @@
+valuable_a=input("문자를 입력하세요:")
+number_a=int(input("문자를 몇 번 출력하시겠습니까?:"))
+result_a=valuable_a*number_a
+print("문자 [{0}]을 {1}번 출력한 결과는 [{2}]입니다.".format(valuable_a,number_a,result_a))
+
